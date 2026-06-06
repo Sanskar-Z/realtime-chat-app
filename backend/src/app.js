74a8db564ae124da9e84e6de6c50c9ede3951ea2
SANSKAR ZINE <sanskarzine21@gmail.com>
2026-06-06 15:52:47 +0530

    feat: initialize Express application with middleware, CORS configuration, and base API routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,19 +2,14 @@ import express from 'express';
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
-
 const app = express()
-
 
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-}));
-
-app.options("*", cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 }));
 
 app.use(express.json({ limit: "16kb" }));
@@ -22,18 +17,12 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"))
 app.use(cookieParser())
 
-
-
 import userRouter from './routes/user.routes.js';
-
 app.use("/api/v1/users", userRouter)
 
-
 import messageRouter from "./routes/message.route.js"
-
 app.use("/api/v1/messages", messageRouter);
 
-// Global error handler (always last)
 app.use((err, req, res, next) => {
   res.status(err.statusCode || 500).json({
     success: false,
